37719205d800866e3fb3c78b5d03f9f4dbc79647
MoonLin <mooncactusmoon@gmail.com>
2021-11-19 14:54:59 +0800

    增加暴力刪除功能

---
+++ member/api/del_user.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+$dsn="mysql:host=localhost;charset=utf8;dbname=member";
+$pdo=new PDO($dsn,'root'.'');
+
+$user_id=$pdo->query("select `id` from `account` where `account`='{$_SESSION['user']}'")->fetchColumn();
+
+$sql_account="DELETE FROM `account` where `id`='{$user_id}'";
+$sql_member="DELETE FROM `member` where `id`='{$user_id}' ";
+
+$pdo->exec($sql_account);
+$pdo->exec($sql_member);
+
+unset($_SESSION['user']);
+header("location:../index.php");
+//刪除的時候特別注意連session跟cookie都要刪除
+
+?>
--- member/include/nav.php
+++ member/include/nav.php
@@ -1,12 +1,20 @@
 <?php
 if(isset($_SESSION['user'])){
-?>    
+?> 
+<style>
+    .del{
+        background: grey;
+        color: white;
+        float:right;
+    }
+</style>   
 <nav>
     <a href="index.php">回首頁</a>
     <a href="dashboard.php">會員中心</a>
     <a href="change_pw.php">修改密碼</a>
     <a href="edit_user.php">編輯會員資料</a>
     <a href="logout.php">登出</a>
+    <a href="./api/del_user.php" class="del">刪除帳號</a>
 </nav>
 <?php }else{ ?>
     <nav>
