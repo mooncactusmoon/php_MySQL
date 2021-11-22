550dd82606db8c2944a9c0babea934ec20100172
MoonLin <mooncactusmoon@gmail.com>
2021-11-22 10:16:18 +0800

    簡易練習 哈瞜人名

--- function/index.php
+++ function/index.php
@@ -5,6 +5,8 @@ echo add(3,5)+10;
 echo "<br>";
 
 $result=add(3,5);
+echo $result+5;
+echo "<br>";
 echo "2" . $result+5;
 echo "<br>";
 echo "result=".$result;
@@ -12,6 +14,14 @@ echo "result=".$result;
 function add($a,$b){
     echo $a+$b;
     return $a+$b;
+}
+echo "<hr>";
+// 寫一個函式 . 可以輸入名字 . 然後 echo "hello XXX"
+
+hello("Moon");
+function hello($name){
+    echo "hello&nbsp;".$name;
+
 }
 
 
