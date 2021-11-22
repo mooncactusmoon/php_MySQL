73dce9f45c06ad1af786df893cbe74fc4e59931d
MoonLin <mooncactusmoon@gmail.com>
2021-11-22 10:41:49 +0800

    stars補充完成

--- function/stars_input.php
+++ function/stars_input.php
@@ -1,4 +1,12 @@
+<!-- action="?"表示回到當前頁面，在此等同於stars_input.php -->
+<form action="?" method="post">
+    <input type="number" name="stars">
+    <input type="submit" value="送出">
+</form>
 <?php
+if(isset($_POST['stars'])){
+    stars($_POST['stars']);
+}
 //三角形星星函式
 function stars($row){
     for($i=1;$i<=$row;$i++){
@@ -10,10 +18,5 @@ function stars($row){
         }
         echo "<br>";
     } 
-    //執行結束
 }
-for($i=5;$i<12;$i=$i+2){
-    stars($i);
-}
-
 ?>
